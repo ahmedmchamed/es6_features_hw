3d3472b5ca4d5bc334e9271a54c220134cee7963
Ahmed Hamed <tunashark@gmail.com>
2020-08-09 00:33:26 +0100

    hw submission

---
+++ book.js
@@ -0,0 +1,10 @@
+export default class Book {
+  author;
+  title;
+  genre;
+  constructor(author, title, genre) {
+    this.author = author;
+    this.title = title;
+    this.genre = genre;
+  }
+}
